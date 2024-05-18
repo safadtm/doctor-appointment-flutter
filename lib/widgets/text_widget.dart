713b26f87d6d69d3a014a3c4923edbb1d57effda
safadtm <safadtm2004@gmail.com>
2024-05-18 10:12:52 +0530

    Splash screen & widgets created

---
+++ lib/widgets/text_widget.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class TextWidget extends StatelessWidget {
+  final String text;
+  final double letterSpace;
+  double size = 5.0;
+  FontWeight fontWeight = FontWeight.normal;
+  final Color color;
+
+  TextWidget(this.text, this.size, this.color, this.fontWeight,
+      {super.key, this.letterSpace = 3});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: color,
+        fontSize: size,
+        fontWeight: fontWeight,
+        letterSpacing: letterSpace,
+      ),
+    );
+  }
+}
